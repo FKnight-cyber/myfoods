47ac4dfa38a29305e429d5bde5c8c414571ec24a
ryannicholas <ryannicholas.vieira@gmail.com>
2022-09-27 20:39:28 -0300

    feat: added errorHandler middleware

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -2,11 +2,13 @@ import express, { json } from "express";
 import "express-async-errors";
 import cors from "cors";
 import mainRouter from "./routers/mainRouter";
+import errorHandler from "./middlewares/errorHandler";
 
 const app = express();
 
 app.use(json());
 app.use(cors());
 app.use(mainRouter);
+app.use(errorHandler);
 
 export default app;
--- back-end/src/services/authService.ts
+++ back-end/src/services/authService.ts
@@ -1,13 +1,20 @@
 import { IUserData, IUserLoginData, IUserInfo } from "../types/authTypes";
 import authRepository from "../repositories/authRepository";
 import { generateUserToken } from "../utils/authUtils";
+import { checkError } from "../middlewares/errorHandler";
 
 async function signUp(user:IUserData) {
-    await authRepository.insert(user)
+    const checkUser = await authRepository.findUser(user.email);
+
+    if(checkUser) throw checkError(409,"Este email já foi registrado!");
+
+    await authRepository.insert(user);
 }
 
 async function signIn(user:IUserLoginData) {
    const checkUser = await authRepository.findUser(user.email);
+
+   if(!checkUser) throw checkError(404,"Este email não está registrado!");
 
    const userInfo:IUserInfo = {
         id:checkUser!.id,
