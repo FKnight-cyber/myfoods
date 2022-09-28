be3dd04ebee3bdc3712eb5f0ed454e0ff2bc56c0
ryannicholas <ryannicholas.vieira@gmail.com>
2022-09-28 17:31:20 -0300

    feat: added logout function to initialpage

--- front-end/src/pages/InitialPage.js
+++ front-end/src/pages/InitialPage.js
@@ -1,16 +1,32 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import styled from "styled-components";
 import logo from "../assets/pizza.png";
 import CategoriesBar from "../components/CategoriesBar";
 import CategoriesMenu from "../components/CategoriesMenu";
 import Products from "../components/Products";
+import { FaSignOutAlt } from "react-icons/fa";
+import UserContext from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
 
 export default function InitialPage(){
     const [category, setCategory] = useState("Pizzas");
     const [openMenu, setOpenMenu] = useState(false);
+
+    const { setToken } = useContext(UserContext);
+    const navigate = useNavigate();
+
     return(
         <InitialPageContainer>
             <header>
+                <FaSignOutAlt 
+                size={30} 
+                color="#ffffff"
+                onClick={()=>{
+                    localStorage.setItem("authToken", "");
+                    setToken("");
+                    navigate("/");
+                }} 
+                />
                 <img src={logo} alt="pizza" srcset="" />
             </header>
             <CategoriesBar 
@@ -42,10 +58,24 @@ const InitialPageContainer = styled.div`
     align-items: center;
 
     header{
+        display: flex;
+        justify-content: center;
+        align-items: center;
         padding-top: 40px;
+        position: relative;
+        width: 100%;
+        
         img{
             width: 106px;
             height: 106px;
+        }
+
+        > * {
+            &:first-child {
+                position: absolute;
+                top: 14px;
+                left: 14px;
+            }
         }
     }
 
