654e35c7bf669ef35a3bb053761d46a7b0e445fa
ryannicholas <ryannicholas.vieira@gmail.com>
2022-09-29 18:24:00 -0300

    feat: added cep delivery validation

--- back-end/src/services/authService.ts
+++ back-end/src/services/authService.ts
@@ -2,11 +2,24 @@ import { IUserData, IUserLoginData, IUserInfo } from "../types/authTypes";
 import authRepository from "../repositories/authRepository";
 import { generateUserToken,encrypt,decrypt } from "../utils/authUtils";
 import { checkError } from "../middlewares/errorHandler";
+import axios from "axios";
 
 async function signUp(user:IUserData) {
     const checkUser = await authRepository.findUser(user.email);
 
     if(checkUser) throw checkError(409,"Este email já foi registrado!");
+
+    const { data:info } = await axios.get(`http://viacep.com.br/ws/${user.cep}/json/`).catch(()=>{
+        throw checkError(500,"Erro na busca pelo seu CEP!");
+    });
+
+    const validAddressDelivery = {
+        Parangaba:true
+    };
+
+    if(info.erro) throw checkError(404,"Não encontramos informação do seu CEP, verifique novamente!");
+
+    if(validAddressDelivery[info.bairro] !== true) throw checkError(401,"Infelizmente não cobrimos o seu bairro ;(");
 
     user.password = encrypt(user.password);
 
